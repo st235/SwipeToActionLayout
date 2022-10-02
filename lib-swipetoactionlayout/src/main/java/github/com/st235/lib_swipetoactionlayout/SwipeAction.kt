bc273c6b7b5762afb5eb0ab3a7b6769d4177a04e
Alexander Dadukin <hi@st235.xyz>
2022-10-02 22:34:23 +0100

    implemented actions builder.

--- lib-swipetoactionlayout/src/main/java/github/com/st235/lib_swipetoactionlayout/SwipeAction.kt
+++ lib-swipetoactionlayout/src/main/java/github/com/st235/lib_swipetoactionlayout/SwipeAction.kt
@@ -4,53 +4,76 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
-import androidx.annotation.*
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.IdRes
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 
 data class SwipeAction internal constructor(
     @IdRes val actionId: Int,
     val background: Drawable?,
     @DrawableRes val iconId: Int,
     val text: CharSequence?,
-    @ColorInt val iconTint: Int = Color.WHITE,
-    @ColorInt val textColor: Int = Color.WHITE
+    @ColorInt val iconTint: Int,
+    @ColorInt val textColor: Int
 ) {
 
-    companion object {
+    constructor(builder: Builder):
+            this(
+                builder.actionId,
+                builder.background,
+                builder.iconId,
+                builder.text,
+                builder.iconTint,
+                builder.textColor
+            )
 
-        fun withBackgroundColor(
-            @IdRes actionId: Int,
-            @DrawableRes iconId: Int,
-            @ColorInt iconTint: Int = Color.WHITE,
-            text: CharSequence?,
-            @ColorInt textColor: Int = Color.WHITE,
-            @ColorInt backgroundColor: Int
-        ): SwipeAction {
-            return SwipeAction(actionId, ColorDrawable(backgroundColor), iconId, text, iconTint, textColor)
+    class Builder(
+        @IdRes internal val actionId: Int = ViewCompat.generateViewId(),
+        @DrawableRes internal val iconId: Int
+    ) {
+
+        internal var background: Drawable? = null
+        internal var text: CharSequence? = null
+        @ColorInt
+        internal var iconTint: Int = Color.WHITE
+        @ColorInt
+        internal var textColor: Int = Color.WHITE
+
+        fun background(background: Drawable): Builder {
+            this.background = background
+            return this
         }
 
-        fun withBackgroundColorRes(
-            context: Context,
-            @IdRes actionId: Int,
-            @DrawableRes iconId: Int,
-            @ColorInt iconTint: Int = Color.WHITE,
-            text: CharSequence?,
-            @ColorInt textColor: Int = Color.WHITE,
-            @ColorRes backgroundColorRes: Int
-        ): SwipeAction {
-            val color = ContextCompat.getColor(context, backgroundColorRes)
-            return SwipeAction(actionId, ColorDrawable(color), iconId, text, iconTint, textColor)
+        fun backgroundRes(context: Context, @DrawableRes backgroundRes: Int): Builder {
+            this.background = ContextCompat.getDrawable(context, backgroundRes)
+            return this
         }
 
-        fun withBackgroundDrawable(
-            @IdRes actionId: Int,
-            @DrawableRes iconId: Int,
-            @ColorInt iconTint: Int = Color.WHITE,
-            text: CharSequence?,
-            @ColorInt textColor: Int = Color.WHITE,
-            background: Drawable?
-        ): SwipeAction {
-            return SwipeAction(actionId, background, iconId, text, iconTint, textColor)
+        fun backgroundColor(@ColorInt color: Int): Builder {
+            this.background = ColorDrawable(color)
+            return this
+        }
+
+        fun text(text: CharSequence): Builder {
+            this.text = text
+            return this
+        }
+
+        fun iconTint(@ColorInt iconTint: Int): Builder {
+            this.iconTint = iconTint
+            return this
+        }
+
+        fun textColor(@ColorInt textColor: Int): Builder {
+            this.textColor = textColor
+            return this
+        }
+
+        fun build(): SwipeAction {
+            return SwipeAction(this)
         }
 
     }
